82e8a0ed68d489449e9e2c60f6497e8187d872a5
Jasper <wellcaffeinated@iCoffee.local>
2013-12-13 19:08:25 -0500

    added code to update next/prev buttons

--- assets/labs-nav.js
+++ assets/labs-nav.js
@@ -20,6 +20,21 @@
         el.className = str;
     }
 
+    function updateNavUrls( prev, next ){
+
+        var nextEl = document.getElementById('lab-nav-next')
+            ,prevEl = document.getElementById('lav-nav-prev')
+            ;
+
+        if ( nextEl && next ){
+            nextEl.setAttribute('href', next.url);
+        }
+
+        if ( prevEl && prev ){
+            prevEl.setAttribute('href', prev.url);
+        }
+    }
+
     if ( el ){
         el.addEventListener('click', function(){
             var wrap = document.getElementById('wrap-outer');
@@ -28,4 +43,22 @@
             return false;
         }, false);
     }
+
+    var labData = {% include labs_list.json %};
+    var curr
+        ,i
+        ,l
+        ,title = window.Lab.title
+        ,id = window.Lab.id
+        ;
+
+    for ( i = 0, l = labData.length; i < l; i++ ){
+        curr = labData[ i ];
+        if ( id === curr.id || title === curr.title ){
+
+            updateNavUrls( labData[ i - 1 ], labData[ i + 1 ] );
+            break;
+        }
+    }
+
 })(this, this.document);
