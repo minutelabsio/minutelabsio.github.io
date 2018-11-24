598fad41745dc3a451b057c709e322bdfdba9d6e
Jasper <well.caffeinated@gmail.com>
2018-11-24 14:11:54 -0500

    remove old code

--- src/lib/image-preloader.js
+++
@@ -1,9 +0,0 @@
-import Promise from 'bluebird'
-export default function( url ){
-  return new Promise((resolve, reject) => {
-    let img = new Image( url )
-    img.onload = () => resolve(img)
-    img.onerror = reject
-    img.src = url
-  })
-}
